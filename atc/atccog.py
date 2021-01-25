dbb03d313f8e51f31871eeb717e68226ae784db2
Keyinator <github@keyinator.net>
2021-01-25 10:23:59 +0100

    Add files via upload

--- atc/atccog.py
+++ atc/atccog.py
@@ -21,43 +21,48 @@ class AtcCog(commands.Cog):
         if ctx.message.author.id in list:
             return
         else:
-            if channel:
-                    await channel.send(content)
-            else:
-                await ctx.send(content)
+            async with ctx.typing():
+                if channel:
+                        await channel.send(content)
+                else:
+                    await ctx.send(content)
 
     @commands.group(pass_context=True)
     @commands.guild_only()
     @commands.mod_or_permissions(manage_messages=True)
     async def blist(self, ctx: commands.Context):
         """Blacklist of whoever should not be able to interact with the bot"""
-        if ctx.invoked_subcommand is None:
-            await ctx.send('Please use either ``add`` or ``rem``')
+        async with ctx.typing():
+            if ctx.invoked_subcommand is None:
+                await ctx.send('Please use either ``add`` or ``rem``')
 
     @blist.command(pass_context=True)
     async def add(self, ctx: commands.Context, user: discord.User):
         """Add Member to the blacklist"""
-        guild_group = self.config.guild(ctx.guild)
-        async with guild_group.blacklisted_users() as list:
-            if user.id in list:
-                await ctx.send('{0} already on blacklist.'.format(user))
-            else:
-                list.append(user.id)
-                await ctx.send('Added {0} to the blacklist'.format(user))
+        async with ctx.typing():
+            guild_group = self.config.guild(ctx.guild)
+            async with guild_group.blacklisted_users() as list:
+                if user.id in list:
+                    await ctx.send('{0} already on blacklist.'.format(user))
+                else:
+                    list.append(user.id)
+                    await ctx.send('Added {0} to the blacklist'.format(user))
 
     @blist.command(pass_context=True)
     async def rem(self, ctx: commands.Context, user: discord.User):
         """Remove Member from the blacklist"""
-        guild_group = self.config.guild(ctx.guild)
-        async with guild_group.blacklisted_users() as list:
-            if user.id in list:
-                list.remove(user.id)
-                await ctx.send('Removed {0} from the blacklist'.format(user))
-            else:
-                await ctx.send('{0} not on the blacklist'.format(user))
+        async with ctx.typing():
+            guild_group = self.config.guild(ctx.guild)
+            async with guild_group.blacklisted_users() as list:
+                if user.id in list:
+                    list.remove(user.id)
+                    await ctx.send('Removed {0} from the blacklist'.format(user))
+                else:
+                    await ctx.send('{0} not on the blacklist'.format(user))
 
     @blist.command(pass_context=True)
     async def list(self, ctx: commands.Context):
         """Output blacklist"""
-        guild_group = await self.config.guild(ctx.guild).blacklisted_users()
-        await ctx.send("{0}".format(guild_group))
+        async with ctx.typing():
+            guild_group = await self.config.guild(ctx.guild).blacklisted_users()
+            await ctx.send("\n".join(str(ctx.bot.get_user(user)) for user in guild_group))
